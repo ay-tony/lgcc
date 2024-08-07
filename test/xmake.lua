a890263abbcd6a262bb4045545c7dd4644b3a03e
aytony <anyang@buaa.edu.cn>
2024-08-07 17:13:31 +0800

    test(xmake): 修复单元测试 xmake 文件中对拍错误的 bug

--- test/xmake.lua
+++ test/xmake.lua
@@ -11,7 +11,6 @@ for _, file in ipairs(os.files("*.c")) do
             import("lib.detect.find_tool")
             local lgcc = path.absolute(path.join(target:targetdir(), "lgcc"))
             local clang = find_tool("clang").program
-            local llc = find_tool("llc").program
 
             -- 在 $(buildir)/.test 下面编译测试点
             import("core.project.config")
@@ -24,14 +23,14 @@ for _, file in ipairs(os.files("*.c")) do
             local clang_out = path.absolute(path.join(testdir, filename .. "-clang"))
 
             -- 测试能否通过编译
-            local lcc_failed = false
+            local lgcc_failed = false
             local clang_failed = false
             try {
                 function ()
                     os.execv(lgcc, { opt.runargs, "-o", lgcc_mid })
-                    os.execv(llc, { lgcc_mid, "-o", lgcc_out })
+                    os.execv(clang, { lgcc_mid, "-o", lgcc_out })
                 end,
-                catch { function () lcc_failed = true end }
+                catch { function () lgcc_failed = true end }
             }
             try {
                 function ()
@@ -40,20 +39,20 @@ for _, file in ipairs(os.files("*.c")) do
                 catch { function () clang_failed = true end }
             } 
 
-            if (lcc_failed and clang_failed) then
+            if (lgcc_failed and clang_failed) then
                 return true
-            elseif (lcc_failed ~= clang_failed) then
+            elseif (lgcc_failed ~= clang_failed) then
                 return false
             end
 
             -- 测试编译后的程序能否正常运行
-            local lcc_output
+            local lgcc_output
             local clang_output
             try {
                 function ()
-                    lcc_output, _ = os.iorunv(lcc_out)
+                    lgcc_output, _ = os.iorunv(lgcc_out)
                 end,
-                catch { function () lcc_failed = true end }
+                catch { function () lgcc_failed = true end }
             } 
             try {
                 function ()
@@ -62,13 +61,13 @@ for _, file in ipairs(os.files("*.c")) do
                 catch { function () clang_failed = true end }
             } 
 
-            if (lcc_failed and clang_failed) then
+            if (lgcc_failed and clang_failed) then
                 return true
-            elseif (lcc_failed ~= clang_failed) then
+            elseif (lgcc_failed ~= clang_failed) then
                 return false
             end
 
-            return lcc_output == clang_output
+            return lgcc_output == clang_output
         end)
 
         on_clean(function (target) 
